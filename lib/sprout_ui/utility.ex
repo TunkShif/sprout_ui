1d6279eeb2ec0fda82269e2905a67d94fdaa9d13
TunkShif <tunkshif@foxmail.com>
2022-10-31 20:12:44 +0800

    refactor: transition component

--- lib/sprout_ui/utility.ex
+++ lib/sprout_ui/utility.ex
@@ -1,18 +1,13 @@
 defmodule SproutUI.Utility do
   use Phoenix.Component
 
-  # FIXME: first render flash
-  # ^ probably fixed now, but not quite satisfied with the current workaround
-
   @default_observed_attribute "data-transition-state"
   @default_observed_states {"show", "hide"}
 
   attr :id, :string, default: "transition-wrapper"
   attr :hook, :string, default: "Transition"
 
-  attr :observing, :list,
-    default: [on: :self, attr: @default_observed_attribute, states: @default_observed_states]
-
+  attr :observing, :list, default: [on: :self]
   attr :initial_state, :string, default: nil
   attr :enter, :string, required: true
   attr :enter_from, :string, required: true
@@ -26,25 +21,26 @@ defmodule SproutUI.Utility do
   slot(:inner_block, required: true)
 
   def transition(assigns) do
-    observing = assigns.observing
     initial_state = assigns.initial_state
     observed_element = assigns.observing[:on]
     observed_attribute = assigns.observing[:attr]
+    observed_states = assigns.observing[:states]
 
-    transition_state = if initial_state && observing[:on] == :self, do: initial_state, else: nil
+    # transition state is only applied when `as_child` is not used
+    transition_state = if initial_state && observed_element == :self, do: initial_state
 
     observing = %{
       on:
         if(observed_element == :self || observed_element == nil,
           do: assigns.id,
-          else: observing[:on]
+          else: observed_element
         ),
-      attribute: observing[:attr] || @default_observed_attribute,
-      state_show: elem(observing[:states] || @default_observed_states, 0),
-      state_hide: elem(observing[:states] || @default_observed_states, 1)
+      attribute: observed_attribute || @default_observed_attribute,
+      state_show: elem(observed_states || @default_observed_states, 0),
+      state_hide: elem(observed_states || @default_observed_states, 1)
     }
 
-    hidden = if initial_state == observing.state_hide, do: true, else: nil
+    hidden = if initial_state == observing.state_hide, do: true
 
     setup = %{
       attrs: %{
@@ -60,7 +56,7 @@ defmodule SproutUI.Utility do
         "data-leave-from" => assigns.leave_from,
         "data-leave-to" => assigns.leave_to,
         "hidden" => hidden,
-        observed_attribute => transition_state
+        observing.attribute => transition_state
       }
     }
 
